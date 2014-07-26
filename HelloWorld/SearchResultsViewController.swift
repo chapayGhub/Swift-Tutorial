33bca770b90161d8dae7d267f0cbeab9b8216ba6
Jameson Quave <jquave@gmail.com>
2014-07-26 18:48:12 -0500

    Part 3

---
+++ HelloWorld/SearchResultsViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jameson Quave on 7/26/14.
+//  Copyright (c) 2014 JQ Software. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol  {
+    
+    @IBOutlet var appsTableView : UITableView?
+    var tableData = []
+    var api = APIController()
+
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.api.delegate = self
+        api.searchItunesFor("Angry Birds")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // The APIControllerProtocol method
+    func didReceiveAPIResults(results: NSDictionary) {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableData = resultsArr
+            self.appsTableView!.reloadData()
+            })
+    }
+
+    
+    // The protocol methods for UITableViewDataSource and UITableViewDelegate
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.textLabel.text = rowData["trackName"] as String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString: NSString = rowData["artworkUrl60"] as NSString
+        let imgURL: NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        let imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.imageView.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel.text = formattedPrice
+        
+        return cell
+    }
+
+}
+
