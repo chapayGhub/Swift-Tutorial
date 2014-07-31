7262ea161426a37d68464ed02d2714984d2a8b5e
Jameson Quave <jquave@gmail.com>
2014-07-30 22:47:24 -0500

    Part6

--- HelloWorld/SearchResultsViewController.swift
+++ HelloWorld/SearchResultsViewController.swift
@@ -49,50 +49,48 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
 
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         
-        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
+    let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
+    
+    let album = self.albums[indexPath.row]
+    cell.textLabel.text = album.title
+    cell.imageView.image = UIImage(named: "Blank52")
+    
+    // Get the formatted price string for display in the subtitle
+    let formattedPrice = album.price
+    
+        // Jump in to a background thread to get the image for this item
         
-        let album = self.albums[indexPath.row]
-        cell.textLabel.text = album.title
-        cell.imageView.image = UIImage(named: "Blank52")
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString = album.thumbnailImageURL
         
-        // Get the formatted price string for display in the subtitle
-        let formattedPrice = album.price
+        // Check our image cache for the existing key. This is just a dictionary of UIImages
+        //var image: UIImage? = self.imageCache.valueForKey(urlString) as? UIImage
+        var image = self.imageCache[urlString]
         
-        dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-            // Jump in to a background thread to get the image for this item
+        if( !image? ) {
+            // If the image does not exist, we need to download it
+            var imgURL: NSURL = NSURL(string: urlString)
             
-            // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
-            let urlString = album.thumbnailImageURL
+            // Download an NSData representation of the image at the URL
+            let request: NSURLRequest = NSURLRequest(URL: imgURL)
+            let urlConnection: NSURLConnection = NSURLConnection(request: request, delegate: self)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
+                if !error? {
+                    image = UIImage(data: data)
+                    
+                    // Store the image in to our cache
+                    self.imageCache["urlString"] = image
+                    cell.imageView.image = image
+                }
+                else {
+                    println("Error: \(error.localizedDescription)")
+                }
+            })
             
-            // Check our image cache for the existing key. This is just a dictionary of UIImages
-            //var image: UIImage? = self.imageCache.valueForKey(urlString) as? UIImage
-            var image = self.imageCache[urlString]
-            
-            if( !image? ) {
-                // If the image does not exist, we need to download it
-                var imgURL: NSURL = NSURL(string: urlString)
-                
-                // Download an NSData representation of the image at the URL
-                let request: NSURLRequest = NSURLRequest(URL: imgURL)
-                let urlConnection: NSURLConnection = NSURLConnection(request: request, delegate: self)
-                NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
-                    if !error? {
-                        image = UIImage(data: data)
-                        
-                        // Store the image in to our cache
-                        self.imageCache["urlString"] = image
-                        cell.imageView.image = image
-                    }
-                    else {
-                        println("Error: \(error.localizedDescription)")
-                    }
-                })
-                
-            }
-            else {
-                cell.imageView.image = image
-            }
-        })
+        }
+        else {
+            cell.imageView.image = image
+        }
         
         cell.detailTextLabel.text = formattedPrice
         
