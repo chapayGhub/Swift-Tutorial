1ed2acfac79c237ef7dfed25a0d4ad67a2e6cbc1
Jameson Quave <jquave@gmail.com>
2014-08-04 22:53:47 -0500

    Part 4

--- HelloWorld/APIController.swift
+++ HelloWorld/APIController.swift
@@ -38,7 +38,7 @@ class APIController {
             }
             var err: NSError?
             var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
-            if(err?) {
+            if(err != nil) {
                 // If there is an error parsing JSON, print it to the console
                 println("JSON Error \(err!.localizedDescription)")
             }
